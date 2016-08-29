bf208b8252d6b75378049fd5765f5b3acf8bef62
pthivierge <pthivierge@osisoft.com>
2016-08-29 13:44:35 +0200

    DataWriter class cleanup

--- DataReader.Core/Workers/DataWriter.cs
+++ DataReader.Core/Workers/DataWriter.cs
@@ -19,41 +19,37 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DataReader.Core.Helpers;
-using log4net;
 using OSIsoft.AF.Asset;
 
 namespace DataReader.Core
 {
     /// <summary>
-    /// This class
+    ///     This class
     /// </summary>
     public class DataWriter : TaskBase, IDisposable
     {
+        public readonly BlockingCollection<IEnumerable<AFValues>> DataQueue =
+            new BlockingCollection<IEnumerable<AFValues>>();
 
-        public readonly BlockingCollection<IEnumerable<AFValues>> DataQueue = new BlockingCollection<IEnumerable<AFValues>>();
-        private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
-        private string _outputFileName;
-        private int _eventsPerFile;
-
-
-        private List<FileWriter> writers = new List<FileWriter>();
+        private readonly List<FileWriter> writers = new List<FileWriter>();
 
 
         public DataWriter(string outputFileName, int eventsPerFile)
         {
-            _outputFileName = outputFileName;
-            _eventsPerFile = eventsPerFile;
+            writers.Add(new FileWriter(eventsPerFile, outputFileName, "1"));
+            writers.Add(new FileWriter(eventsPerFile, outputFileName, "2"));
+            writers.Add(new FileWriter(eventsPerFile, outputFileName, "3"));
+            writers.Add(new FileWriter(eventsPerFile, outputFileName, "4"));
+        }
 
-            writers.Add(new FileWriter(eventsPerFile, outputFileName , "1"));
-            writers.Add(new FileWriter(eventsPerFile, outputFileName , "2"));
-            writers.Add(new FileWriter(eventsPerFile, outputFileName , "3"));
-            writers.Add(new FileWriter(eventsPerFile, outputFileName , "4"));
+
+        public void Dispose()
+        {
         }
 
         public override void Stop()
@@ -64,59 +60,58 @@ namespace DataReader.Core
 
         protected override void DoTask(CancellationToken cancelToken)
         {
-            WriteData(_cancellationToken.Token, _outputFileName);
-        }
-
-        private void WriteData(CancellationToken cancelToken, string fileName)
-        {
-
-
             _logger.InfoFormat("Writing data task started...");
 
 
             // gets currently available values from the queue
-            foreach (IEnumerable<AFValues> valuesList in DataQueue.GetConsumingEnumerable(cancelToken))
+            foreach (var valuesList in DataQueue.GetConsumingEnumerable(cancelToken))
             {
+                // find an available writer to write the results into a file
+                var writer =
+                    writers.FirstOrDefault(
+                        w =>
+                            w.ActiveTask == null ||
+                            (w.ActiveTask.IsCompleted && w.ActiveTask.Status != TaskStatus.WaitingForActivation));
 
 
-                
-                var writer = writers.FirstOrDefault(w => w.ActiveTask == null || (w.ActiveTask.IsCompleted && w.ActiveTask.Status != TaskStatus.WaitingForActivation));
-
+                // incase no writer was available, null was returned, here we wait until we get an available writer
                 if (writer == null)
                 {
                     Task.WaitAny(writers.Select(w => w.ActiveTask).ToArray());
-                    writer = writers.FirstOrDefault(w => w.ActiveTask == null || (w.ActiveTask.IsCompleted && w.ActiveTask.Status != TaskStatus.WaitingForActivation));
+                    writer =
+                        writers.FirstOrDefault(
+                            w =>
+                                w.ActiveTask == null ||
+                                (w.ActiveTask.IsCompleted && w.ActiveTask.Status != TaskStatus.WaitingForActivation));
                 }
 
 
-                writer.ActiveTask = Task.Run(() =>
+                // safety check to avoid null, at this point this should neve occur
+                if (writer != null)
                 {
-                    foreach (AFValues afValues in valuesList)
+
+                    writer.ActiveTask = Task.Run(() =>
                     {
-                        foreach (AFValue afValue in afValues)
+                        foreach (var afValues in valuesList)
                         {
+                            foreach (var afValue in afValues)
+                            {
+                            
                             // if you need to check data - we can do it here
-                            // 
 
                             var line = afValue.Timestamp.LocalTime + "," + afValue.Value + "," + afValue.PIPoint.Name;
-                            writer.WriteLine(line);
+                                writer.WriteLine(line);
+                            }
                         }
-                    }
-                });
-
+                    }, cancelToken);
+                }
+                else
+                {
+                    _logger.Error("DataWriter encounterd a null, FileWriter.  This situation should never occur, please report the issue.");
+                }
             }
 
             _logger.InfoFormat("Datawriter completed.");
-        }
-
-
-
-
-
-
-        public void Dispose()
-        {
-
         }
     }
 }
